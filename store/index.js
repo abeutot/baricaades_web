86ada7c515f91765236c3413ab6d2e10e6544d6b
Anaël Beutot <abeutot@oserror.org>
2020-12-14 01:57:38 +0100

    Add a quick credentials check when rendering the page server side
    If the credentials are not valid, we can ask the user for new
    registering.

--- store/index.js
+++ store/index.js
@@ -1,10 +1,33 @@
 import { parse as parseCookie } from 'cookie'
+import { btoa } from 'js-base64'
 
 export const actions = {
-  nuxtServerInit({ commit }, { req }) {
+  async nuxtServerInit({ dispatch }, { req }) {
     const cookies = parseCookie(req.headers.cookie || '')
     if (cookies.username && cookies.token) {
-      commit('auth/set', { username: cookies.username, token: cookies.token })
+      await dispatch('loadCredentials', {
+        username: cookies.username,
+        token: cookies.token,
+      })
     }
+  },
+  async loadCredentials({ commit }, { username, token }) {
+    console.log('will check credentials')
+    /* check credentials are valid */
+    try {
+      const resp = await this.$axios.$get('/user/check', {
+        headers: {
+          'Authorization': 'Basic ' + btoa(username + ':' + token),
+        },
+      })
+    } catch (err) {
+      if (err.response !== undefined && err.response.status === 401) {
+        return
+      }
+
+      throw err
+    }
+
+    commit('auth/set', { username: username, token: token })
   },
 }
